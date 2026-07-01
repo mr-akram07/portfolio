0521a8f7d1ff26ce953fbf7de254ca82a5973131
Mohammad Akram <makram4653@gmail.com>
2026-07-01 22:35:22 +0530

    New Project Added

--- data.js
+++ data.js
@@ -44,6 +44,7 @@ export const skills = [
 ];
 
 export const projects = [
+  { title: "E Commerce Website", link: "https://abs-kohl-nu.vercel.app/", desc: "I created a modern e-commerce website for a local business.", image: "./ECommerce.png", tags: ["Shop Website","E-commerce","Online Store"], },
   { title: "MCQ Test Website", link: "https://ccc-blush.vercel.app/", desc: "A website is created for a preparation of any MCQ exam which is managed by admin.", image: "./CCC.png", tags: ["Exam Preparation", "MCQ Tests", "Quick Mock Tests"], },
   { title: "Portfolio Website", link: "", desc: "A personal portfolio built with React and Tailwind CSS.", image: "./test.png", tags: ["Resume", "Branding"], },
   
